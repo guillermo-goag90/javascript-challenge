2eed5944ccac1edfaf3e2f21e072de57aa7eadcd
Rusty Ryan <guillermo.goag@gmail.com>
2020-10-01 15:57:24 -0400

    test event handler function

--- static/js/app.js
+++ static/js/app.js
@@ -2,28 +2,6 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-// var cities = [];
-
-// function print(city) {
-//     console.log(city);
-// }
-
-
-
-// tableData.forEach(function(element) {
-//     cities.push(element.city)
-// })
-
-// cities.forEach(print)
-
-// var ufoArray = Object.values(tableData);
-// console.log(typeof ufoArray);
-// console.log(ufoArray);
-// console.log(tableData);
-
-// Object.values(tableData).forEach(observation => {
-//     console.log(observation.state)
-// })
 
 // Select first two elements
 var obsArray = [];
@@ -32,7 +10,7 @@ for (i = 0; i < 3; i++) {
 }
 
 console.log(obsArray);
-console.log(typeof obsArray);
+//console.log(typeof obsArray);
 
 var selTable = d3.select("tbody");
 
@@ -43,3 +21,72 @@ var selTable = d3.select("tbody");
      })
  })
     
+// jalar toda la data
+var drill = tableData.filter(entry => entry.datetime < "1/10/2010");
+//console.log(drill)
+
+var newArray = [];
+for (i = 0; i < 3; i++) {
+    newArray.push(tableData[i]);
+}
+
+// Create handlers
+var selTable = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var input = d3.select(".form-control");
+
+//input.on("change", FUNCTION)
+
+
+//var results = [];
+function handleChange (event) {
+    var inputDate = d3.event.target.value
+    var formatDate = new Date(inputDate)
+    
+    console.log(inputDate)
+    console.log(formatDate)
+    
+    // Loop through data and filter based on date
+    var results = [];
+    tableData.forEach(entry => {
+        var entryDate = new Date(entry.datetime);
+
+        if (entryDate <= formatDate) {
+            results.push(entry);
+        }
+    })
+    
+    //
+    results.forEach(result => {
+        var tableRow = selTable.append("tr");
+        results.forEach(d => {
+            tableRow.append("td").text(d);
+        })
+    })
+}
+
+input.on("change", handleChange);
+
+//console.log(results.length)
+
+// input.on("change", () => {
+//     var drill = d3.event.target.value;
+//     console.log(drill);
+// })
+
+
+var endDate = new Date("01/04/2010");
+
+// Convert string to date
+function strToDate (str) {
+    var newDate = new Date(string);
+    return newDate;
+}
+
+// tableData.forEach(entry => {
+//     var entryDate = new Date(entry.datetime);
+    
+//     if (entryDate <= formatDate) {
+//         results.push(entry);
+//     }
+// })
